4aaffe144e0a6aedb6e9dc856d7785f63c4e3d89
Soumen Das <sdas721444@gmail.com>
2025-07-19 13:29:41 +0530

    Implement Navbar component and update Home route layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,25 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Welcome to Resumind!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
+       <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>Review your Resume and Check AI-Powered Suggestions</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
+
